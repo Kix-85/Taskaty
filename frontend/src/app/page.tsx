fe3c461ffcb17e47d5405be6aead70b9fb934900
Kix-85 <kareemaldeeb25@gmail.com>
2025-03-24 19:39:39 +0200

    Working Version

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { console } from "inspector";
+
+const s:number=5;
+console.log(s)
+
 
 export default function Home() {
   return (
-    <></>
+    <><h1>Hello TypeScript</h1></>
   );
 }
