f56f724837fba71c0454b6e3f0034c451bf22f5e
YousefEbad <yousefomar6464@gmail.com>
2025-04-04 20:40:25 +0200

    login and registration beta version

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,130 @@
-import { console } from "inspector";
+"use client";
+import TextField from "@/components/textField";
+import {SearchField} from "@/components/textField";
+import styles from "./page.module.css";
+import myLogo from "../../public/assets/imgs/logo.png";
+import userAccount from "../../public/assets/imgs/userAccount.png";
+import downArrow from "../../public/assets/imgs/downArrow.png";
+import googleIcon from "../../public/assets/imgs/googleIcon.png";
+import Image from "next/image";
+import "./styles/global.css";
+import { useState } from "react";
 
-const s:number=5;
-console.log(s)
+// Welcome Back 
+// Dear Friend
 
+// Sign In To Your Account
 
 export default function Home() {
+
+  const [isRegistered, setIsRegistered] = useState(false); // State to track registration status
+  const handleRegister = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    console.log("Register button clicked");
+    console.log(isRegistered);
+    setIsRegistered((prev) => !prev); // Toggle the registration status
+  };
   return (
-    <><h1>Hello TypeScript</h1></>
+    <>
+      <div className={styles.registerPage}>
+        <header>
+          <div className={styles.leftHeaderPart}>
+            <div className="logo"><Image 
+                    className="myLogo" 
+                    src={myLogo} 
+                    alt="clear input"
+                    width={118.2}     
+                />
+            </div>
+            <ul className={styles.headerList}>
+              <li><a href="#">overview</a></li>
+              <li><a href="#">Features</a></li>
+              <li><a href="#">Pricing</a></li>
+              <li><a href="#">About</a></li>
+            </ul>
+          </div>
+          <div className={styles.rightHeaderPart}>
+            <div className={styles.userLogo}><Image 
+                    className="userAccount" 
+                    src={userAccount} 
+                    alt="clear input"
+                    width={22}
+                />
+            </div>
+            <h4>Account</h4>
+            <div className={styles.downArrow}>
+              <Image 
+                    className="downArrow" 
+                    src={downArrow} 
+                    alt="clear input"
+                    width={22}
+                />
+            </div>
+          </div>
+        </header>
+        <main>
+          <div className={styles.mainContainer}>
+            <div className={styles.leftMainPart}>
+              <div className={styles.content}>
+              <h1>
+                {isRegistered ? "Welcome Back" : "Join Over 2568+"}  
+                <br></br>  
+                <span>{isRegistered ? "Dear Friend" : "Expert Designers"}</span>
+              </h1>
+                <p>We take pride in offering unparalleled customer support to ensure your experience with our platforms</p>
+              </div>
+              <div className="card">
+
+              </div>
+            </div>
+            <div className={styles.rightMainPart}>
+            <h2>{isRegistered ? "Sign In To Your Account" : "Register Your Account"}</h2>
+              <form>
+                {!isRegistered && (
+                <div className={styles.labelContainer}>
+                  <label className={styles.label}>First Name:
+                    <TextField placeholder={"Enter your First Name"}/>
+                  </label>
+                </div>
+                )}
+                {!isRegistered && (
+                <div className={styles.labelContainer}>
+                  <label className={styles.label}>Last Name:
+                    <TextField placeholder={"Enter your Last Name"}/>
+                  </label>
+                </div>
+                )}
+                <div className={styles.labelContainer}>
+                  <label className={styles.label}>Email:
+                    <TextField placeholder={"Enter your Email"}/>
+                  </label>
+                </div>
+                
+                <div className={styles.labelContainer}>
+                  <label className={styles.label}>Password:
+                    <TextField placeholder={"Enter your Password"}/>
+                  </label>
+                </div>
+              
+                <button className={styles.registerButton}>
+                  {isRegistered ? "want to Register Account" : "want to Login"}
+                </button>
+                <p>{isRegistered ? "You don't have an account - ":"Already Have An Account - "}<button onClick={handleRegister}>{isRegistered ? "Register Here" : "Sign In Here"}</button></p>
+                <button className={styles.googleRegisterButton}><Image 
+                    className="googleIcon" 
+                    src={googleIcon} 
+                    alt="clear input"
+                    width={20}
+                    height={20}
+                />SignUp With Google Account
+                </button>
+              </form>
+            </div>
+          </div>
+        </main>
+        {/* <TextField/>
+        <SearchField /> */}
+      </div>
+    </>
   );
 }
