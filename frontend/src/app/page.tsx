0bbeb0532db9ed9b72d93b6dc50503a166aacf22
Kix-85 <kareemaldeeb25@gmail.com>
2025-04-15 21:46:33 +0200

    myTasks added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,10 @@
 "use client";
+
 export default function Home() {
 
-  
   return (
     <>
-    {/*leave it empty
-        use it only for trial
-        dont push anything in here
-        ok?!!!!!!!!
-        Read fools
-    */}
+      {/*don't play here*/}
     </>
   );
 }
