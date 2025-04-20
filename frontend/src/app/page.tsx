cd578d4414591eca4eda5053bacf34b6f27b1763
YousefEbad <yousefomar6464@gmail.com>
2025-04-20 18:37:57 +0200

    TextField Component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,8 @@
 import "./globals.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faHeart, faCircleXmark } from '@fortawesome/free-solid-svg-icons'; // Import specific icons
+import {SearchField} from "@/components/textField/textField"
+import TextField from "@/components/textField/textField";
 
 export default function Home() {
   return (
