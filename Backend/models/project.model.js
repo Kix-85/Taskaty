416b5e046c42b9da89df32ecd691e18a03fdb4f3
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-05-08 21:44:52 +0300

    add the project's model

---
+++ Backend/models/project.model.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 100
+    },
+    logo : {
+        type: String,
+        required: false,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: 1000
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['not_started', 'in_progress', 'done', 'just_started'],
+        default: 'not_started'
+    },
+    activity: [{
+        type: String,
+        required: false,
+        trim: true,
+        maxlength: 1000,
+        date: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+    lastViews : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users',
+        required: false
+    }],
+    dueDate: {
+        type: Date,
+        required: false
+    },
+    leader: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    teamMembers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+}, { timestamps: true });
+
+const Project = mongoose.model('Projects', projectSchema);
+module.exports = Project;
