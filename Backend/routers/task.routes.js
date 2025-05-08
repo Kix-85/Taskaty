1edee98f01d0cb51571f81b436390fb174c493ec
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-05-08 21:46:43 +0300

    add basic task routes

--- Backend/routers/task.routes.js
+++ Backend/routers/task.routes.js
@@ -1,12 +1,13 @@
-// routes/tasks.js
 const express = require('express');
 const router = express.Router();
-const tasksController = require('../controllers/task.controller');
+const { getAllTasks, createTask, updateTask, deleteTask, searchTask } = require('../controllers/task.controller');
 
-router.get('/', tasksController.getAllTasks);
-router.post('/', tasksController.createTask);
-router.put('/:id', tasksController.updateTask);
-router.delete('/:id', tasksController.deleteTask);
-router.get('/search', tasksController.searchTask);
+router.get('/:userID', getAllTasks);
+router.post('/:userID', createTask);
+router.get('/:taskID', getTask); // Assuming you have a function to get a task by ID
+router.put('/:taskID', updateTask);
+router.delete('/:taskID', deleteTask);
+// apply search if there is a query string in the url  (I will do it later) or search by name only still not implemented
+// router.get('/search/:taskID', searchTask);
 
 module.exports = router;
