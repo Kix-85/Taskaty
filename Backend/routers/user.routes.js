694d77509cffd111e4883011611828348717954e
Ayman-Elfeky <167029914+Ayman-Elfeky@users.noreply.github.com>
2025-05-08 21:48:18 +0300

    modify user routes and controllers for search only

--- Backend/routers/user.routes.js
+++ Backend/routers/user.routes.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers, createUser, updateUser, deleteUser, searchUser } = require('../controllers/user.controller');
+const { searchUser } = require('../controllers/user.controller');
 
-router.get('/', getAllUsers);
-
-router.post('/', createUser);
-
-router.put('/:id', updateUser);
-
-router.delete('/:id', deleteUser);
 
 router.get("/search", searchUser);
 
